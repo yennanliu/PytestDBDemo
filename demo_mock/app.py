255695926e28386fc8f5015db04f4cd5193aba73
yennanliu <f339339@gmail.com>
2020-04-05 13:32:15 +0800

    add test_with_mock.py and its to-test func : call_website_if_status (app.py)

--- demo_mock/app.py
+++ demo_mock/app.py
@@ -5,5 +5,13 @@ def call_website():
     response = requests.get(url)
     return response
 
+def call_website_if_status():
+    url = "https://www.python.org/"
+    response = requests.get(url)
+    if requests.exceptions.Timeout:
+        return None
+    return response
+
 def get_website_data(response):
     return response.text
+    
--- demo_mock/test_with_mock.py
+++ demo_mock/test_with_mock.py
@@ -1,8 +1,9 @@
 import pytest 
 import unittest
 from unittest.mock import Mock, patch
+from requests.exceptions import Timeout
 # UDF 
-from app import call_website
+from app import call_website, call_website_if_status
 
 class TestApp(unittest.TestCase):
     def test_call_website(self):
@@ -10,6 +11,16 @@ class TestApp(unittest.TestCase):
             mock_requests.get.return_value = "123"
             response = call_website()
             assert response == "123"
+
+    def test_call_website_if_status(self):
+        with patch('app.requests') as mock_requests:
+            mock_requests.get.status_code = 200
+            mock_requests.get.side_effect = Timeout
+            with self.assertRaises(Timeout):
+                response = call_website_if_status()
+                ### TO FIX : mock_requests.get_call_count assertion 
+                #assert mock_requests.get_call_count == 2
+                assert response == None
 
 if __name__ == '__main__':
     pytest.main([__file__])
